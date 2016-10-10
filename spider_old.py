79c65d33c1771df2c0d9647c0de1bb71b28efc43
LoonLi <l470279614@yahoo.com>
2016-10-10 22:43:35 +0800

    ver1.1

--- spider_old.py
+++ spider_old.py
@@ -5,20 +5,20 @@ import time
 from bs4 import BeautifulSoup
 
 def makeRequest(url):
-	req = urllib2.Request(url)
-	#request.add_header('Accept-Language','zh;q=0.8')
-	req.add_header('Cookie','p_ab_id=9; login_ever=yes; stacc_mode=unify; a_type=0; is_sensei_service_user=1; bookmark_tag_type=count; bookmark_tag_order=desc; _ga=GA1.2.744023963.1467183331; PHPSESSID=14695102_c391ab8482a4da2d71713de84b831752; module_orders_mypage=%5B%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22hot_entries%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; ki_t=1467183360295%3B1476101051384%3B1476105098045%3B16%3B32; ki_r=; __utma=235335808.744023963.1467183331.1476096197.1476103654.35; __utmb=235335808.8.10.1476103654; __utmc=235335808; __utmz=235335808.1474680004.24.7.utmcsr=weibo.com|utmccn=(referral)|utmcmd=referral|utmcct=/u/2032349783/home; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=14695102=1')
+	request = urllib2.Request(url)
+	request.add_header('Accept-Language','zh;q=0.8')
 	# request.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
 	# request.add_header('Accept-Encoding','gzip, deflate, sdch')
 	# request.add_header('Connection','keep-alive')
 	# request.add_header('Cookie','p_ab_id=9; login_ever=yes; stacc_mode=unify; a_type=0; is_sensei_service_user=1; bookmark_tag_type=count; bookmark_tag_order=desc; _ga=GA1.2.744023963.1467183331; module_orders_mypage=%5B%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22hot_entries%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; ki_t=1467183360295%3B1476055411949%3B1476097632750%3B15%3B28; ki_r=; PHPSESSID=14695102_c391ab8482a4da2d71713de84b831752; __utmt=1; __utma=235335808.744023963.1467183331.1476078305.1476096197.34; __utmb=235335808.73.9.1476097675591; __utmc=235335808; __utmz=235335808.1474680004.24.7.utmcsr=weibo.com|utmccn=(referral)|utmcmd=referral|utmcct=/u/2032349783/home; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=14695102=1')
 	# request.add_header('Host','www.pixiv.net')
 	# request.add_header('Upgrade-Insecure-Requests','1')
-	#request.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36')
-	return urllib2.urlopen(req)
+	request.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36')
+	return urllib2.urlopen(request)
+
 
 #construct a url
-tag = "まったく、駆逐艦は最高だぜ!!"
+tag = "ナネットさん"
 origin_url = 'http://www.pixiv.net/search.php?s_mode=s_tag&word='
 url = origin_url+urllib2.quote(tag,"!")
 #print tag
@@ -29,7 +29,6 @@ resp = makeRequest(url)
 #print resp.url
 
 item_urls = []
-scores_board = {}
 next_url = url
 page_count = 1
 
@@ -39,29 +38,52 @@ while True:
 	items=soup.select('li[class="image-item"]')
 	for item in items:
 		item_url = "http://www.pixiv.net"+item.a['href']
-		a_count = item.select('a[class="bookmark-count _ui-tooltip"]')
-		if len(a_count)!=0:
-			item_count = int(a_count[0].text)
-		else:
-			item_count = 0
-		scores_board[item_url] = item_count
+		item_urls.append(item_url)
 	print next_url,"has finished.This is no.",page_count,"."
+	#get next page url
+	# a=soup.select('a[r/el="next"]')
 	
+	#limit the pages
+	# if page_count%5==0:
+	# 	print "Let me have a rest..."
+	# 	time.sleep(5)
+
 	if page_count>100:
 		break
 	page_count+=1
-	
+
+	# if len(a)!=0:
+	# 	a=a[0]
+	# 	href=a['href']
+	# 	next_url="http://www.pixiv.net/search.php"+href
+	# 	resp=makeRequest(next_url)
+	# else:
+	# 	break
 	if len(items)<10:
 		break
 	#print len(items)
-	
 	next_url = url+"&p="+str(page_count)
 	resp = makeRequest(next_url)
 
 
 
+print "Finished!!"
+# print "we get these urls:"
+# for x in item_urls:
+# 	print x
+# print len(item_urls)
 print "-------------------------------------------------"
 print "Staring scores collecting."
+
+scores_board = {}
+
+for item_url in item_urls:
+	resp = makeRequest(item_url)
+	soup = BeautifulSoup(resp.read(),'html.parser')
+	span = soup.select('span[class="views"]')
+	scores = int(span[1].string)
+	scores_board[item_url] = scores
+	print item_url,"has [[[",scores,"]]] scores."
 
 scores_board = sorted(scores_board.iteritems(), key=lambda d:d[1], reverse = True)
 
